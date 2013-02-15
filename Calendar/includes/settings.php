ac9b42e067193cadb0238ff2687314d526a3378c
unknown <Sean@.(none)>
2013-02-14 22:16:41 -0800

    Added calendar, synced

---
+++ Calendar/includes/settings.php
@@ -0,0 +1,15 @@
+<?php
+/* updated via install/index.php on Thu, 01 Mar 2012 13:05:16 -0500
+install_password: 5f4dcc3b5aa765d61d8327deb882cf99
+db_type: mysql
+db_host: localhost
+db_database: letmrack_wbc2
+db_login: letmrack_wbc2
+db_password: TxIASLrYkf
+db_persistent: false
+readonly: false
+user_inc: user.php
+use_http_auth: false
+single_user: false
+# end settings.php */
+?>
