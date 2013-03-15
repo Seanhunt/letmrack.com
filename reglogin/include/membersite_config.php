70d0238d54d1dca6d6ff3e9935d63ecc784191ee
unknown <Sean@.(none)>
2013-03-15 16:03:50 -0700

    moved reglogin code up, ready to expose, database name changed to letmrack_shows

--- reglogin/include/membersite_config.php
+++ reglogin/include/membersite_config.php
@@ -4,23 +4,23 @@ require_once("./include/fg_membersite.php");
 $fgmembersite = new FGMembersite();
 
 //Provide your site name here
-$fgmembersite->SetWebsiteName('user11.com');
+$fgmembersite->SetWebsiteName('letmrack.com');
 
 //Provide the email address where you want to get notifications
-$fgmembersite->SetAdminEmail('user11@user11.com');
+$fgmembersite->SetAdminEmail('admin@letmrack.com');
 
 //Provide your database login details here:
 //hostname, user name, password, database name and table name
 //note that the script will create the table (for example, fgusers in this case)
 //by itself on submitting register.php for the first time
 $fgmembersite->InitDB(/*hostname*/'localhost',
-                      /*username*/'prasanth',
-                      /*password*/'p',
-                      /*database name*/'testdb',
-                      /*table name*/'fgusers3');
+                      /*username*/'letmrack_sean',
+                      /*password*/'gwalchmai',
+                      /*database name*/'letmrack_shows',
+                      /*table name*/'users');
 
 //For better security. Get a random string from this link: http://tinyurl.com/randstr
 // and put it here
-$fgmembersite->SetRandomKey('qSRcVS6DrTzrPvr');
+$fgmembersite->SetRandomKey('OVYvHz2gYLMIyLe');
 
 ?>
